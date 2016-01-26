0751a6e049d75efa6d181b88d09629b613b45e7b
D-Pow <devon.powell12@ncf.edu>
2016-01-25 20:07:06 -0500

    Create Tuple.java

---
+++ src/Tuple.java
@@ -0,0 +1,19 @@
+package invadersofspace;
+
+class Tuple{
+    private final double x;
+    private final double y;
+    
+    public Tuple(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX(){
+        return this.x;
+    }
+    
+    public double getY(){
+        return this.y;
+    }
+}
